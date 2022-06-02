d4e3cd8829c4fa750e48d7775e8b7d9b2bb817c5
srihari <sriharimuppalla64@gmail.com>
2022-06-02 05:01:04 +0000

    GET Todo API

--- app.js
+++ app.js
@@ -26,3 +26,108 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
+
+const convertData = (data) => {
+  return {
+    id: data.id,
+    todo: data.status,
+    category: data.category,
+    priority: data.priority,
+    status: data.status,
+    dueDate: data.due_date,
+  };
+};
+
+const hasPriorityAndStatusProperties = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  );
+};
+
+//Getting Todos with Different Scenarios
+
+app.get("/todos/", async (request, response) => {
+  const {
+    status = "",
+    search_q = "",
+    category = "",
+    priority = "",
+    dueDate = "",
+  } = request.query;
+  const requestQuery = request.query;
+  let getTodosQuery = "";
+  let data = null;
+  switch (true) {
+    case requestQuery.status !== undefined:
+      getTodosQuery = `
+            SELECT
+            *
+            FROM
+            todo
+            WHERE
+            status = "${requestQuery.status}";
+            `;
+      data = await database.all(getTodosQuery);
+      if (data.length > 0) {
+        response.send(data.map((each) => convertData(each)));
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Status");
+      }
+      break;
+    case requestQuery.priority !== undefined:
+      getTodosQuery = `
+            SELECT
+            *
+            FROM
+            todo
+            WHERE
+            priority = "${requestQuery.priority}";
+            `;
+      data = await database.all(getTodosQuery);
+      if (data.length > 0) {
+        response.send(data.map((each) => convertData(each)));
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Priority");
+      }
+      break;
+    case hasPriorityAndStatusProperties(requestQuery):
+      getTodosQuery = `
+            SELECT
+            *
+            FROM
+            todo
+            WHERE
+            priority = "${requestQuery.priority}"
+            AND status = "${requestQuery.status}";
+            `;
+      data = await database.all(getTodosQuery);
+      if (data.length > 0) {
+        response.send(data.map((each) => convertData(each)));
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Status");
+      }
+      break;
+    case requestQuery.priority !== undefined:
+      getTodosQuery = `
+            SELECT
+            *
+            FROM
+            todo
+            WHERE
+            priority = "${requestQuery.priority}";
+            `;
+      data = await database.all(getTodosQuery);
+      if (data.length > 0) {
+        response.send(data.map((each) => convertData(each)));
+      } else {
+        response.status(400);
+        response.send("Invalid Todo Priority");
+      }
+      break;
+    default:
+      break;
+  }
+});
